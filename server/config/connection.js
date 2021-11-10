84b42f8a4d90c881983a9ca1e3aa05bec60b3965
Khanh Truong <khanhtruong_1992@yahoo.com>
2021-11-09 23:58:26 -0800

    some changes

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/refactoring', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/refactoring-redux-platform', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
